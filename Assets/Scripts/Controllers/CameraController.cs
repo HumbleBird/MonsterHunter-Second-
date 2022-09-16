48f938ada9e5e205d7682cdcfe79c5c01a5b4249
HumbleBird <wltnsdid67@naver.com>
2022-09-16 17:45:40 +0900

    캐릭터 애니메이션 기본 토대 끝

--- Assets/Scripts/Controllers/CameraController.cs
+++ Assets/Scripts/Controllers/CameraController.cs
@@ -11,7 +11,12 @@ public class CameraController : MonoBehaviour
     Vector3 _delta = new Vector3(0.0f, 6.0f, -5.0f);
 
     [SerializeField]
-    GameObject _player = null;
+    Transform _player = null;
+
+    [SerializeField] float m_Speed;
+    [SerializeField] float m_MaxRayDist = 1;
+    [SerializeField] float m_Zoom = 3f;
+    Vector2 m_Input;
 
     void Start()
     {
@@ -23,22 +28,71 @@ public class CameraController : MonoBehaviour
         if (_mode == Define.CameraMode.QuarterView)
         {
             RaycastHit hit;
-            if (Physics.Raycast(_player.transform.position, _delta, out hit, _delta.magnitude, LayerMask.GetMask("Wall")))
+            if (Physics.Raycast(_player.position, _delta, out hit, _delta.magnitude, LayerMask.GetMask("Wall")))
             {
-                float dist = (hit.point - _player.transform.position).magnitude * 0.8f;
-                transform.position = _player.transform.position + _delta.normalized * dist;
+                float dist = (hit.point - _player.position).magnitude * 0.8f;
+                transform.position = _player.position + _delta.normalized * dist;
             }
             else
             {
-                transform.position = _player.transform.position + _delta;
-                transform.LookAt(_player.transform);
+                transform.position = _player.position + _delta;
+                transform.LookAt(_player);
             }
         }
+
+        Rotate();
     }
 
     public void SetQuarterView(Vector3 delta)
     {
         _mode = Define.CameraMode.QuarterView;
         _delta = delta;
+    }
+
+    void Rotate()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            m_Input.x = Input.GetAxis("Mouse X");
+            m_Input.y = Input.GetAxis("Mouse Y");
+
+            if (m_Input.magnitude != 0)
+            {
+                Quaternion q = _player.rotation;
+                q.eulerAngles = new Vector3(q.eulerAngles.x + m_Input.y * m_Speed, q.eulerAngles.y + m_Input.x * m_Speed, q.eulerAngles.z);
+                _player.rotation = q;
+
+            }
+        }
+    }
+
+    void Zoom()
+    {
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        if (scroll != 0)
+        {
+            Transform cam = Camera.main.transform;
+            if (CheckRay(cam, scroll))
+            {
+                Vector3 targetDist = cam.transform.position - _player.transform.position;
+                targetDist = Vector3.Normalize(targetDist);
+                Camera.main.transform.position -= (targetDist * scroll * m_Zoom);
+            }
+        }
+
+        Camera.main.transform.LookAt(_player.transform);
+    }
+
+    bool CheckRay(Transform cam, float scroll)
+    {
+        if (Physics.Raycast(cam.position, transform.forward, out RaycastHit m_Hit, m_MaxRayDist))
+        {
+            Debug.Log("hit point : " + m_Hit.point + ", distance : " + m_Hit.distance + ", name : " + m_Hit.collider.name);
+            Debug.DrawRay(cam.position, transform.forward * m_Hit.distance, Color.red);
+            cam.position += new Vector3(0, 0, m_Hit.point.z);
+            return false;
+        }
+
+        return true;
     }
 }
